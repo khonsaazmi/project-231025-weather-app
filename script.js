978beb975b082de8cc0bfe36c347945addfd1598
Khonsa <khonsaazmi@gmail.com>
2025-10-23 23:16:22 +0700

    Update script

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const card = document.querySelector('.card');
 const apiKey = "74660a88f88aee5deebfbb4c3c84ba46";
 
 // EVENT LISTENERS
-weatherForm.addEventListener("submit", event => {
+weatherForm.addEventListener("submit", async event => {
 
     event.preventDefault(); // prevent form from submitting
 
@@ -15,19 +15,35 @@ weatherForm.addEventListener("submit", event => {
 
     // check if city is valid
     if (city) {
-
+        try {
+            const weatherData = await getWeatherData(city); // get weather data
+            displayWeatherInfo(weatherData); // display weather info
+        }
+        catch (error) {
+            console.error(error); // log error
+            displayError(error.message); // display error
+        }
     }
     else {
-        displayError("Please enter a city!");
+        displayError("Please enter a city!"); // display error
     }
 });
 
 async function getWeatherData(city) {
 
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+
+    const response = await fetch(apiUrl); // fetch weather data
+
+    if (!response.ok) { // check if response is ok  
+        throw new Error("Could not fetch weather data"); // throw error
+    }
+
+    return await response.json();
 }
 
 function displayWeatherInfo(data) {
-
+    console.log(data);
 }
 
 function getWeatherEmoji(weatherId) {
