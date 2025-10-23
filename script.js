8700408d55f9fd67fa177ff01c12348baee12544
Khonsa <khonsaazmi@gmail.com>
2025-10-23 23:53:01 +0700

    Add script for the display in card from city to emoji

--- script.js
+++ script.js
@@ -36,18 +36,68 @@ async function getWeatherData(city) {
     const response = await fetch(apiUrl); // fetch weather data
 
     if (!response.ok) { // check if response is ok  
-        throw new Error("Could not fetch weather data"); // throw error
+        throw new Error("Error: Could not fetch weather data"); // throw error
     }
 
     return await response.json();
 }
 
 function displayWeatherInfo(data) {
-    console.log(data);
+
+    const {
+        name: city, // get city
+        main: { temp, humidity }, // get temperature and humidity
+        weather: [{ description, id }] } = data; // get description and id
+
+    card.textContent = ""; // clear card
+    card.style.display = "flex"; // show card
+
+    const cityDisplay = document.createElement('h1'); // create city display
+    const tempDisplay = document.createElement('p'); // create temperature display
+    const humidityDisplay = document.createElement('p'); // create humidity display
+    const descDisplay = document.createElement('p'); // create description display
+    const weatherEmoji = document.createElement('p'); // create weather emoji
+
+    cityDisplay.textContent = city; // set city
+    tempDisplay.textContent = `${(temp - 273.15).toFixed(1)}°C`; // set temperature from kelvin to celsius
+    humidityDisplay.textContent = `Humidity: ${humidity}%`; // set humidity
+    descDisplay.textContent = description; // set description
+    weatherEmoji.textContent = getWeatherEmoji(id); // set weather emoji from id
+
+    cityDisplay.classList.add('cityDisplay'); // add city display class
+    tempDisplay.classList.add('tempDisplay'); // add temperature display class
+    humidityDisplay.classList.add('humidityDisplay'); // add humidity display class
+    descDisplay.classList.add('descDisplay'); // add description display class
+    weatherEmoji.classList.add('weatherEmoji'); // add weather emoji class
+
+    card.appendChild(cityDisplay); // add city display to card
+    card.appendChild(tempDisplay); // add temperature display to card
+    card.appendChild(humidityDisplay); // add humidity display to card
+    card.appendChild(descDisplay); // add description display to card
+    card.appendChild(weatherEmoji); // add weather emoji to card
 }
 
 function getWeatherEmoji(weatherId) {
 
+    // get weather emoji from id
+    switch (true) {
+        case (weatherId >= 200 && weatherId <= 232): // thunderstorm
+            return "⛈️";
+        case (weatherId >= 300 && weatherId <= 321): // drizzle
+            return "🌧️";
+        case (weatherId >= 500 && weatherId <= 531): // rain
+            return "🌧️";
+        case (weatherId >= 600 && weatherId <= 622): // snow
+            return "❄️";
+        case (weatherId >= 701 && weatherId <= 781): // atmosphere
+            return "🌫️";
+        case (weatherId === 800): // clear
+            return "☀️";
+        case (weatherId >= 801 && weatherId <= 804): // clouds
+            return "☁️";
+        default:
+            return "❓"; // unknown weather
+    }
 }
 
 function displayError(message) {
